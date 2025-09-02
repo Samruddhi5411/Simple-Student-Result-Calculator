16d658e721f5f7f6deaf723a9d8ce442efe48070
Samruddhi5411 <samruddhimagar202@gmail.com>
2025-09-02 09:44:33 +0530

    Student Name

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Mini__Project {
+}
---
+++ src/practice/Result_cal.java
@@ -0,0 +1,23 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Result_cal {
+
+	public static void main(String[] args) {
+//	    String studentName;
+//	    int rollNo;
+	    System.out.println("Enter Student Name");
+		Scanner sc = new Scanner(System.in);
+		String studentName = sc.next();
+		System.out.println("Student Name : " + studentName);
+		
+		 
+		 
+	        
+	       
+		
+       
+	}
+
+}
